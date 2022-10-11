e8e20e4aad519351d19765fa4b1cca17ae969c00
Raman yadav <40426625+RamanDonald@users.noreply.github.com>
2022-10-11 10:14:38 +0530

    #include<iostream> using namespace std; int main() {     int n;     cin>>n;     int arr[n];     for(int i=0;i<n;i++)     {         cin>>arr[i];     }     int c=0;     for(int i=0;i<n;i++)     {
           c=c+arr[i];
           
        }
        int d=0;
        for(int i=0;i<n;i++)
        {
            if(arr[i]<=(c/n))
            {
                d=d+1;
            }
        }
        cout<<d;
    }

---
+++ elements_smaller_than_average.c
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int c=0;
+    for(int i=0;i<n;i++)
+    {
+       
+       c=c+arr[i];
+       
+    }
+    int d=0;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]<=(c/n))
+        {
+            d=d+1;
+        }
+    }
+    cout<<d;
+}
